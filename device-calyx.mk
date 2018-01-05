dabda4b76b325b15cea1ef2c219d5d4d5d5e74ec
Abhisek Devkota <ciwrl@lineageos.org>
2018-01-04 21:19:09 -0800

    Enable EUICC Sim
    Change-Id: If43e859c80e69b35b08c1097ad268cf4411d2d8b

---
+++ device-calyx.mk
@@ -0,0 +1,3 @@
+# EUICC feature
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.telephony.euicc.xml
