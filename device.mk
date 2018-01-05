e9f362a801e778f8b3b8d2b59db55c922e439eef
Abhisek Devkota <ciwrl@lineageos.org>
2018-01-04 21:19:09 -0800

    Enable EUICC Sim
    Change-Id: If43e859c80e69b35b08c1097ad268cf4411d2d8b

--- device.mk
+++ device.mk
@@ -17,6 +17,8 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+$(call inherit-product, device/google/wahoo/device-calyx.mk)
+
 ifneq (,$(filter 27, $(PRODUCT_EXTRA_VNDK_VERSIONS)))
     _vndk_test := true
 endif
