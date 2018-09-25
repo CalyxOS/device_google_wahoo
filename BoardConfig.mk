82b224f14237ab0a5eede90398b0983bdaa56e02
Chirayu Desai <chirayudesai1@gmail.com>
2018-09-25 16:08:54 +0530

    Configure for inline kernel building
    Based on LineageOS makefiles
    
    Change-Id: Ia8a2d013a462d27fb38963e74926ffddb1ca35d6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,7 +79,6 @@ BOARD_PERSISTIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 # DTBO partition definitions
-BOARD_PREBUILT_DTBOIMAGE := device/google/wahoo-kernel/dtbo.img
 BOARD_DTBOIMG_PARTITION_SIZE := 8388608
 
 TARGET_COPY_OUT_VENDOR := vendor
@@ -180,3 +179,5 @@ AB_OTA_PARTITIONS += \
     system \
     vbmeta \
     dtbo
+
+include device/google/wahoo/BoardConfigCalyx.mk
