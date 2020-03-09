4ca79996dde70f14a16f2e72962a35b69b3e5ab8
Abhisek Devkota <ciwrl@lineageos.org>
2020-03-09 22:14:05 +1100

    Build RCS packages
    Change-Id: I9e482b9c35131e3634aaf7a6c8ba05ade11b28fb

--- device-calyx.mk
+++ device-calyx.mk
@@ -4,3 +4,9 @@ DEVICE_PACKAGE_OVERLAYS += device/google/wahoo/overlay-calyx
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.telephony.euicc.xml \
     device/google/wahoo/permissions/privapp-permissions-aosp-extended.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-aosp-extended.xml
+
+# RCS
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager \
+    RcsService \
+    PresencePolling
