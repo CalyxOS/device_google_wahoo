ee06553b6b311383e47ef1a477ba420466adcc15
Chirayu Desai <chirayudesai1@gmail.com>
2020-07-15 04:08:24 +0530

    Add privapp-permissions for EUICC

--- device.mk
+++ device.mk
@@ -720,3 +720,5 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.telephony.euicc.xml
 
+PRODUCT_COPY_FILES += \
+    device/google/wahoo/permissions/privapp-permissions-aosp-extended.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-aosp-extended.xml
