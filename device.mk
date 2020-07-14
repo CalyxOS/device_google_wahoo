838771488f48d23fdfc1da3e02e9aa687a36aa41
Chirayu Desai <chirayudesai1@gmail.com>
2020-07-15 04:08:24 +0530

    Add privapp-permissions for EUICC
    Change-Id: I8e6915940f3479f22e601a36d66bc8509069da88

--- device.mk
+++ device.mk
@@ -745,3 +745,6 @@ include hardware/google/pixel/thermal/device.mk
 # EUICC feature
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.telephony.euicc.xml
+
+PRODUCT_COPY_FILES += \
+    device/google/wahoo/permissions/privapp-permissions-aosp-extended.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-aosp-extended.xml
