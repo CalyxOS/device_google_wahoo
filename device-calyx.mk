a0669026d2972d08e23d0d4f714aab19be8546ec
Chirayu Desai <chirayudesai1@gmail.com>
2020-11-19 02:20:02 +0530

    Set vendor SPL to 2020-10-05
    * Pixel 2 devices didn't get the November 2020 security update
    * They did get the December 2020 update as their last update,
      however Google didn't bump the SPL in that build, so let's
      just leave this to what they last shipped.
    
    Change-Id: I8529bb119dcfe6614e68bad5bce8307e04f99c40

--- device-calyx.mk
+++ device-calyx.mk
@@ -4,3 +4,6 @@ DEVICE_PACKAGE_OVERLAYS += device/google/wahoo/overlay-calyx
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.telephony.euicc.xml \
     device/google/wahoo/permissions/privapp-permissions-aosp-extended.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-aosp-extended.xml
+
+# Setting vendor SPL
+VENDOR_SECURITY_PATCH := 2020-10-05
