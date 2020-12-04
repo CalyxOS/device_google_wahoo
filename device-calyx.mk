4fa20a0ec0e26e98a27281e780d3f8fa78f07c42
Chirayu Desai <chirayudesai1@gmail.com>
2020-12-05 04:47:54 +0530

    Add overlay-calyx to the list of overlays
    Change-Id: Ie34758123c979caefd7c7abc62180543755137e4

--- device-calyx.mk
+++ device-calyx.mk
@@ -1,3 +1,5 @@
+DEVICE_PACKAGE_OVERLAYS += device/google/wahoo/overlay-calyx
+
 # EUICC feature
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.telephony.euicc.xml \
