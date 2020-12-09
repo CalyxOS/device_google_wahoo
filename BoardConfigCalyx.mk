ba2e8c2f43a1829eda8347e39aefb772aaf32c4f
Chirayu Desai <chirayudesai1@gmail.com>
2020-12-09 05:43:20 +0530

    Squashed merge of r25
    Change-Id: I7a6277f33cea28b0da9def323d306b52c76f14d3

---
+++ BoardConfigCalyx.mk
@@ -0,0 +1,10 @@
+# Inline kernel building
+BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+    DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc \
+    MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_SOURCE := kernel/google/wahoo
+TARGET_NEEDS_DTBOIMAGE := true
+TARGET_KERNEL_CONFIG := wahoo_defconfig
