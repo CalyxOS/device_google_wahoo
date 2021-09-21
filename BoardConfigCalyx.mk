e3f5b969c1995a8ef1f96e1e25aaa6bd32ceb3f8
Michael Bestas <mkbestas@gmail.com>
2021-09-21 16:35:56 +0300

    wahoo: Set firmware and kernel security patch levels
    * vendor/firmware come from last available stock update level, which is October 2020.
    * kernel gets updated with latest monthly ASB patches and android-4.4-p
      updates, so it matches platform security patch level.
    
    Change-Id: Icc46b2bfacb49eb390acd1f3a45c63564d3a02df

--- BoardConfigCalyx.mk
+++ BoardConfigCalyx.mk
@@ -10,3 +10,6 @@ TARGET_KERNEL_SOURCE := kernel/google/wahoo
 TARGET_NEEDS_DTBOIMAGE := true
 TARGET_KERNEL_CONFIG := wahoo_defconfig
 endif
+
+# Properties
+TARGET_VENDOR_PROP += device/google/wahoo/vendor.prop
